d3bdbaabe4412c9c6f41c52934bdcab74c62724b
Ainhoa <Ainhoa@Ainhoa>
2016-05-28 18:28:39 +0200

    Creación de un nuevo objeto Calculadora en la clase principal y comienzo con la ventana calculadora

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -5,10 +5,79 @@
  */
 package calculadora;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.WindowConstants;
+import javax.swing.border.EmptyBorder;
+
 /**
  *
  * @author Ainhoa
  */
-public class Calculadora {
+public class Calculadora extends JFrame{
     
+    /**
+	 * generado
+	 */
+	private static final long serialVersionUID = 1583724102189855698L;
+
+	/** numero tecleado */
+	JTextField pantalla;
+
+	/** guarda el resultado de la operacion anterior o el número tecleado */
+	double resultado;
+
+	/** para guardar la operacion a realizar */
+	String operacion;
+
+	/** Los paneles donde colocaremos los botones */
+	JPanel panelNumeros, panelOperaciones;
+
+	/** Indica si estamos iniciando o no una operación */
+	boolean nuevaOperacion = true;
+
+        /**
+	 * Constructor. Crea los botones y componentes de la calculadora
+	 */
+	public Calculadora() {
+		super();
+		setSize(250, 300);
+		setTitle("Calculadora Simple");
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setResizable(false);
+
+		// Vamos a dibujar sobre el panel
+		JPanel panel = (JPanel) this.getContentPane(); // sin esta linea no funciona
+		panel.setLayout(new BorderLayout());
+
+		pantalla = new JTextField("0", 20);
+		pantalla.setBorder(new EmptyBorder(4, 4, 4, 4));
+		pantalla.setFont(new Font("Arial", Font.BOLD, 25));
+		pantalla.setHorizontalAlignment(JTextField.RIGHT);
+		pantalla.setEditable(false);
+		pantalla.setBackground(Color.WHITE);
+		panel.add("North", pantalla);
+
+		panelNumeros = new JPanel();
+		panelNumeros.setLayout(new GridLayout(4, 3));
+		panelNumeros.setBorder(new EmptyBorder(4, 4, 4, 4));
+
+		
+
+		panel.add("Center", panelNumeros);
+
+		panelOperaciones = new JPanel();
+		panelOperaciones.setLayout(new GridLayout(6, 1));
+		panelOperaciones.setBorder(new EmptyBorder(4, 4, 4, 4));
+
+
+		panel.add("East", panelOperaciones);
+
+		validate();
+	}
 }
--- src/calculadora/Principal.java
+++ src/calculadora/Principal.java
@@ -16,6 +16,9 @@ public class Principal {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Calculadora calculadora = new Calculadora();
+        calculadora.setVisible(true);
     }
     
 }
